afe40bde611c36a6da9d34e346fd940a6ca25d84
maxiaoepng <xiaopeng.ma@beigenecorp.net@uw2c-rstud-p.beigenecorp.net>
2020-09-29 01:24:55 -0700

    update data description

--- R/data.R
+++ R/data.R
@@ -1,11 +1,12 @@
 #' clinical and bimarker data of Imvigor210
 #'
-#' A dataset containing the clinical and gene expression variabels
+#' A dataset containing the clinical and gene expression variables
 #'
 #' @format A data frame with 348 observations on the following 17965 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{gep_}{gene expressions}
+#'   \item{gepscore_}{gene signature score}
+#'   \item{mut_}{gene mutation}
 #'   ...
 #' }
 #' @source \url{http://research-pub.gene.com/IMvigor210CoreBiologies/packageVersions/IMvigor210CoreBiologies_1.0.0.tar.gz}
